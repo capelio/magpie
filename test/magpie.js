bebb68039d2a43333c600025d494911da431267c
C. Corey Capel <corey@capelio.com>
2013-05-13 16:25:01 -0400

    Refactor tests a bit.

--- test/magpie.js
+++ test/magpie.js
@@ -17,10 +17,15 @@ var theOatmealBook = {
 	author: "TheOatmeal.com"
 };
 
-var theOatmealBookWithId = _.clone(theOatmealBook);
-theOatmealBookWithId = {
+var bookWithId = {
 	id: "978-1449410247"
 };
+bookWithId = _.merge(theOatmealBook, bookWithId);
+
+var updatedBook = {
+	isbn10: "1449410243"
+};
+updatedBook = _.merge(bookWithId, updatedBook);
 
 /**
  * Tests
@@ -36,6 +41,16 @@ describe('Magpie', function() {
 	});
 
 	describe('#create', function() {
+		// TODO: it should
+		// - return a InvalidArguments error if the record is not an object
+
+		it('should return an error if the record is not an object', function(done) {
+			db.create('this is not an object', function(error, record) {
+				should.exist(error);
+				done();
+			});
+		});
+
 		it('should create an ID when one is not provided', function(done) {
 			db.create(theOatmealBook, function(error, record) {
 				record.should.have.property('id');
@@ -44,37 +59,45 @@ describe('Magpie', function() {
 		});
 
 		it('should use an existing ID when one is provided', function(done) {
-			db.create(theOatmealBookWithId, function(error, record) {
-				record.should.have.property('id', theOatmealBookWithId.id);
+			db.create(bookWithId, function(error, record) {
+				record.should.have.property('id', bookWithId.id);
 				done();
 			});
 		});
 	});
 
 	describe('#get', function() {
+		// TODO: it should
+		// - return a InvalidArguments error when improperly called
+		// - look at idThatDoesNotExist again, should we test for the existence of an error? also, do we need
+		//   to test when there is as well as when there isn't a callback?
+
 		it('should return a record when querying for an ID as a string', function(done) {
-			db.get(theOatmealBookWithId.id, function(error, record) {
-				record.should.have.property('id', theOatmealBookWithId.id);
+			db.get(bookWithId.id, function(error, record) {
+				record.should.have.property('id', bookWithId.id);
 				done();
 			});
 		});
 
 		it('should not return a record when querying for a non-existant ID as a string', function(done) {
-			db.get('idThatDoesNotExist', function(error, record) {
+			db.get('doesNotExist', function(error, record) {
+				if (error) throw error;
 				should.not.exist(record);
 				done();
 			});
 		});
 
 		it('should return a record when querying for an ID on a query object', function(done) {
-			db.get({id: theOatmealBookWithId.id}, function(error, record) {
-				record.should.have.property('id', theOatmealBookWithId.id);
+			db.get({id: bookWithId.id}, function(error, record) {
+				if (error) throw error;
+				record.should.have.property('id', bookWithId.id);
 				done();
 			});
 		});
 
 		it('should not return a record when querying for a non-existant ID on a query object', function(done) {
-			db.get({id: 'idThatDoesNotExist'}, function(error, record) {
+			db.get({id: 'doesNotExist'}, function(error, record) {
+				if (error) throw error;
 				should.not.exist(record);
 				done();
 			});
@@ -82,6 +105,7 @@ describe('Magpie', function() {
 
 		it('should return all records when only supplying a callback', function(done) {
 			db.get(function(error, records) {
+				if (error) throw error;
 				records.should.not.be.empty;
 				done();
 			});
